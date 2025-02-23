218012e975028ebf7b40840b69aab7ba1f825f2d
Guido Schmutz <schmutz68@gmail.com>
2025-02-23 14:24:05 +0100

    add additional environment variables for OLLAMA_BASE_URL and OLLAMA_MODEL

--- src/assistant/configuration.py
+++ src/assistant/configuration.py
@@ -16,10 +16,10 @@ class SearchAPI(Enum):
 class Configuration:
     """The configurable fields for the research assistant."""
     max_web_research_loops: int = 3
-    local_llm: str = "llama3.2"
+    local_llm: str = os.environ.get("OLLAMA_MODEL", "llama3.2")
     search_api: SearchAPI = SearchAPI.DUCKDUCKGO  # Default to DUCDUCKGO
     fetch_full_page: bool = False  # Default to False
-    ollama_base_url: str = "http://localhost:11434/"
+    ollama_base_url: str = os.environ.get("OLLAMA_BASE_URL", "http://localhost:11434/")
 
     @classmethod
     def from_runnable_config(
